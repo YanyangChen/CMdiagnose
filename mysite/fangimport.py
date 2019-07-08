9bec881ff4a907e221dcda209beac55df1a3ed6e
YanyangChen <184536632@qq.com>
2019-07-08 16:17:34 +0800

    added wang and not finished wangscript

--- mysite/fangimport.py
+++ mysite/fangimport.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 from CMdiagnose.models import Cases
 
-for i in range(3, 5):
-    page = requests.get("http://yibian.hopto.org/fang/?fno=" + str(i), proxies={'http': 'proxy5.edb.gov.hk:8080'},timeout=5)
+for i in range(601, 9271):
+    page = requests.get("http://yibian.hopto.org/wang/?wno=" + str(i), proxies={'http': 'proxy5.edb.gov.hk:8080'},timeout=5)
     soup = BeautifulSoup(page.content, 'html.parser')
     n=str(soup)[int(str(soup).find("方名")):]
-    name_=n[int(n.find("<th align=\"left\">")): int(n.find("</th>"))].replace('<th align="left">','')
+    name_=n[int(n.find("方名</td>\n<th align=\"left\" nowrap=\"\">")): int(n.find("</th>"))].replace('方名</td>\n<th align=\"left\" nowrap=\"\">','')
     zf=str(soup)[int(str(soup).find("【製法用量】")): int(str(soup).find("【功效】"))]
     zf=zf[int(zf.find("【製法用量】")): int(zf.find("。<p>"))].replace('<p>','')
     x=str(soup)[int(str(soup).find("【功效】")): int(str(soup).find("【主治】"))]
@@ -23,3 +23,4 @@ for i in range(3, 5):
     fcts=smpt.replace('【功效】','').replace('【主治】','').replace('者','').replace('，',',').replace('。',',').replace('、',',').replace('<p>','')
     y=Cases(solution=name_+" "+zf, facts=fcts, symptom=smpt, reference=fy+xd)
     y.save()
+    print(str(i) + " finished")
