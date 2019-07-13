5d44269ce2c70f7f7f573b7c326ec14c59e88c07
chenyanyang <184536632@qq.com>
2019-07-13 13:53:44 +0800

    added search in admin

--- mysite/CMdiagnose/admin.py
+++ mysite/CMdiagnose/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
 from .models import Tongue, Body, Person, Cases, Yao, Xue
+# https://stackoverflow.com/questions/28512710/how-to-add-custom-search-box-in-django-admin
+class CasesAdmin(admin.ModelAdmin):
 
+    list_display=('pk','solution','symptom','facts')
+    search_fields = ('solution', 'symptom', 'facts')
 admin.site.register(Person)
 admin.site.register(Tongue)
 admin.site.register(Body)
-admin.site.register(Cases)
+# admin.site.register(Cases)
 admin.site.register(Yao)
 admin.site.register(Xue)
+admin.site.register(Cases, CasesAdmin)
+
+
 
